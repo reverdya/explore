fb2c1a7f3a2be370a4dcedc3b3448dcbaa0fc531
ALIX REVERDY <reverdy.alix@gmail.com>
2022-04-20 18:34:17 +0200

    ggplot QUALYPSO functions and tas_global modification

--- general_functions.R
+++ general_functions.R
@@ -1,6 +1,5 @@
 # Alix Reverdy
 # Explore 2
-# 04/04/2022
 # general data science function and data representations
 
 
@@ -187,23 +186,29 @@ base_map_outlets=function(data,val_name){
   return(plt)
 }
 
-#####
-##IPCC colors
+##########################################
+##IPCC colors and others
 
+#For continuous variable requiring good distinction
 yellow_blue_5=c(rgb(255,255,204,maxColorValue=255),rgb(161,218,180,maxColorValue=255),rgb(65,182,196,maxColorValue=255),rgb(44,127,184,maxColorValue=255),rgb(37,52,148,maxColorValue=255))
+#For Precipitation
 precip_5=c(rgb(166,97,26,maxColorValue=255),rgb(223,194,125,maxColorValue=255),rgb(245,245,245,maxColorValue=255),rgb(128,205,193,maxColorValue=255),rgb(1,133,113,maxColorValue=255))
+#For temperature
 temp_5=c(rgb(202,0,32,maxColorValue=255),rgb(244,165,130,maxColorValue=255),rgb(247,247,247,maxColorValue=255),rgb(146,197,222,maxColorValue=255),rgb(5,113,176,maxColorValue=255))
+#For rcp
 col_3rcp=c("#0000FF","#79BCFF","#FF0000")
+
 
 ###################################
 ## Format Global temperature for use in Qualypso, to be used inside code run_QUalypso
-## Difference to 1850-1900 average and rcp/gcm matching
+## Difference to 1850-1900 average and rcp/gcm matching + spline smoothing
 ## path_data the root of the path
 ##simu_lst the list of simulations
 #first_full year and last_full_year the first an last years with data for all simu all year round
 
-format_global_tas=function(path_data,first_full_year,last_full_year,simu_lst){
+format_global_tas=function(path_data,first_full_year,last_full_year,simu_lst,first_ref_year,last_ref_year){
   
+  vecYears=seq(first_full_year,last_full_year,1)
   ## Format global temperature: difference to 1850-1900 
   paths=list.files(paste0(path_data,"raw/Global_temp/"),pattern=glob2rx("global_tas*"),full.names = T)
   for ( i in 1:length(paths)){
@@ -227,8 +232,295 @@ format_global_tas=function(path_data,first_full_year,last_full_year,simu_lst){
   for (i in 1:nrow(simu_lst)){
     mat_Globaltas[[i]]=mat_Globaltas_gcm[,which(vec_global_tas_gcm==simu_lst[i,]$gcm & vec_global_tas_rcp==sub(".","",simu_lst[i,]$rcp,fixed=T))]
   }
-  mat_Globaltas=t(do.call(cbind,mat_Globaltas))
-  return(mat_Globaltas)
+  mat_Globaltas=do.call(cbind,mat_Globaltas)
+  mat_Globaltas=t(apply(mat_Globaltas,MARGIN = 2,function(x) smooth.spline(x=vecYears,y=x,spar = 1)$y))
+  ref_Globaltas=apply(mat_Globaltas,MARGIN = 1,function(x) mean(x[which(vecYears==first_ref_year):which(vecYears==last_ref_year)]))
+  return(list(mat_Globaltas,ref_Globaltas))
 }
 
 
+##############################################################################
+## Time series of main effect from QUALYPSOOUT object or main effect + mu
+##returns a ggplot 2 object
+## QUALYPSOOUT a Qualypso output object
+## iEff the number of the given effect in QUALYPSOOUT
+## CIlevel the uncertainties percentiles
+## includeMean if True then adds mean change to effect
+## eff_name the plain language name of the effect
+## pred_name the plain language name of the predictor
+## ind_name the plain language name of the indicator
+## bv_name the plain language name of the watershed if needed
+## ind_unit the unit of the indicator
+## pre_unit the unit of the predictor
+
+plotQUALYPSOeffect_ggplot=function(QUALYPSOOUT,iEff,CIlevel=c("5%","95%"),includeMean=FALSE,eff_name,pred_name,ind_name,bv_name,ind_unit,pred_unit,folder_out){
+  
+  # vector of predictors
+  Xfut = QUALYPSOOUT$Xfut
+  
+  # retrieve effects
+  if(includeMean){
+    QUANTEff = QUALYPSOOUT$CHANGEBYEFFECT[[iEff]]$QUANT
+  }else{
+    QUANTEff = QUALYPSOOUT$MAINEFFECT[[iEff]]$QUANT
+  }
+  nEff = dim(QUANTEff)[3]
+  
+  # find index quantiles
+  iMedian = which(colnames(QUANTEff)=="50%")
+  iBinf = which(colnames(QUANTEff)==CIlevel[1])
+  iBsup = which(colnames(QUANTEff)==CIlevel[2])
+  if((length(iBinf)!=1)|(length(iBsup)!=1)) stop(paste0('Quantiles ',CIlevel, "are not available, check argument CIlevel"))
+  
+  # retrieve median and limits
+  medRaw = QUANTEff[,iMedian,]
+  binfRaw = QUANTEff[,iBinf,]
+  bsupRaw = QUANTEff[,iBsup,]
+  
+  # get smooth limits
+  med = data.frame(apply(medRaw,2,function(x) predict(loess(x~Xfut))))
+  binf = data.frame(apply(binfRaw,2,function(x) predict(loess(x~Xfut))))
+  bsup = data.frame(apply(bsupRaw,2,function(x) predict(loess(x~Xfut))))
+  
+  # Format for ggplot
+  colnames(med)=QUALYPSOOUT$listScenarioInput$listEff[[iEff]]
+  colnames(binf)=QUALYPSOOUT$listScenarioInput$listEff[[iEff]]
+  colnames(bsup)=QUALYPSOOUT$listScenarioInput$listEff[[iEff]]
+  med$pred=Xfut
+  binf$pred=Xfut
+  bsup$pred=Xfut
+  med=gather(med,key = "eff",value = "med",-pred)
+  binf=gather(binf,key = "eff",value = "binf",-pred)
+  bsup=gather(bsup,key = "eff",value = "bsup",-pred)
+  data=merge(med,binf,by=c("pred","eff"))
+  data=merge(data,bsup,by=c("pred","eff"))
+  
+  plt=ggplot(data)+
+    geom_ribbon(aes(x=pred,ymin=binf,ymax=bsup,fill=eff),alpha=0.3)+
+    geom_line(aes(x=pred,y=med,group=eff,color=eff),size=1)+
+    geom_line(aes(x=pred,y=binf,group=eff,color=eff),size=0.5,linetype="dotted")+
+    geom_line(aes(x=pred,y=bsup,group=eff,color=eff),size=0.5,linetype="dotted")+
+    scale_x_continuous(paste0(pred_name," (",pred_unit,")"))+
+    theme_bw(base_size = 18)+
+    theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
+    theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))
+    
+  if(colnames(QUALYPSOOUT$listScenarioInput$scenAvail)[iEff]=="rcp"){
+    plt=plt+
+      scale_color_discrete("Mediane lissee avec\nintervalle 5-95%",type = col_3rcp,labels=labels_rcp)+
+      scale_fill_discrete("Mediane lissee avec\nintervalle 5-95%",type=col_3rcp,labels=labels_rcp)
+  }else{
+    plt=plt+
+      scale_color_discrete("Mediane lissee avec\nintervalle 5-95%",type = parula(nEff))+
+      scale_fill_discrete("Mediane lissee avec\nintervalle 5-95%",type=parula(nEff))
+  }
+  
+  if(includeMean){
+    plt=plt+
+      scale_y_continuous(paste0("Changement (",ind_unit,")"))+
+      ggtitle(paste0("Changement des ",eff_name," pour le predicteur ",pred_name,"\net l'indicateur ",ind_name," (",bv_name,")"))
+    save.plot(plt,Filename = paste0("change_",ind_name,"_",eff_name,"_",pred_name,"_",bv_name),Folder = folder_out,Format = "jpeg")
+  }else{
+    plt=plt+
+      scale_y_continuous(paste0("Effet principal (",ind_unit,")"))+
+      ggtitle(paste0("Effet principaux des ",eff_name," pour le predicteur ",pred_name,"\net l'indicateur ",ind_name," (",bv_name,")"))
+    save.plot(plt,Filename = paste0("effect_",ind_name,"_",eff_name,"_",pred_name,"_",bv_name),Folder = folder_out,Format = "jpeg")
+  }
+  
+}
+
+
+##############################################################################
+## Time series of mu + uncertainties
+## returns a ggplot 2 object
+## QUALYPSOOUT a Qualypso output object (effects order should be coherent with color coding inside this function)
+## pred_name the plain language name of the predictor
+## ind_name the plain language name of the indicator
+## bv_name the plain language name of the watershed if needed
+## ind_unit the unit of the indicator
+## pred_unit the unit of the predictor
+
+plotQUALYPSOMeanChangeAndUncertainties_ggplot=function(QUALYPSOOUT,pred_name,ind_name,bv_name,ind_unit,pred_unit,folder_out){
+  
+  probCI = QUALYPSOOUT$listOption$probCI
+  Xfut = QUALYPSOOUT$Xfut
+  nFut = length(Xfut)
+  meanPred = QUALYPSOOUT$GRANDMEAN$MEAN
+  meanPred=predict(loess(meanPred ~ Xfut))
+  vNorm = t(QUALYPSOOUT$DECOMPVAR)
+  Vtot = QUALYPSOOUT$TOTALVAR
+  nEff = nrow(vNorm) - 2
+  vNormRev = apply(vNorm, 2, rev)
+  binf = qnorm(p = (1 - probCI)/2, mean = meanPred, sd = sqrt(Vtot))
+  bsup = qnorm(p = 0.5 + probCI/2, mean = meanPred, sd = sqrt(Vtot))
+  limIntInf = limIntSup = matrix(nrow = nEff + 2, ncol = nFut)
+  limIntInf[1, ] = predict(loess(binf ~ Xfut))
+  limIntSup[1, ] = predict(loess(bsup ~ Xfut))
+  for (i in 1:(nEff + 1)) {
+    binfi = limIntInf[i, ] + vNormRev[i, ] * (meanPred - 
+                                                binf)
+    limIntInf[i + 1, ] = predict(loess(binfi ~ Xfut))
+    bsupi = limIntSup[i, ] - vNormRev[i, ] * (bsup - meanPred)
+    limIntSup[i + 1, ] = predict(loess(bsupi ~ Xfut))
+  }
+  
+  namesEff = QUALYPSOOUT$names
+  names_var=c("int","res",rev(namesEff))
+
+  
+  data=data.frame(Xfut=Xfut,mean=meanPred)
+  data=cbind(data,t(limIntInf))
+  colnames(data)[-c(1:2)]=names_var
+  data2=gather(data,key = "var",value = "inf",-c(Xfut,mean))
+  data=data.frame(Xfut=Xfut)
+  data=cbind(data,t(limIntSup))
+  colnames(data)[-c(1)]=names_var
+  data3=gather(data,key = "var",value = "sup",-Xfut)
+  data=merge(data2,data3,by=c("Xfut","var"))
+  data$var=factor(data$var,levels=c(names_var))
+  
+  #These colors work only if the order of the effects does not move
+  col_7var=c("orange","yellow", "cadetblue1", "blue1","darkgreen", "darkgoldenrod4","darkorchid1")
+  legend_var=c("Var. Int.","Var. Res.","RCM","GCM","RCP","BC","HM")
+  
+  plt=ggplot(data)+
+    geom_ribbon(aes(x=Xfut,ymin=inf,ymax=sup,fill=var),alpha=0.8)+
+    geom_line(aes(x=Xfut,y=mean,color="black"))+
+    scale_fill_discrete("Partition\nde la variance",type = col_7var[1:length(names_var)],labels=legend_var[1:length(names_var)])+
+    scale_color_discrete("",type="black",label="Moyenne lissee")+
+    scale_x_continuous(paste0(pred_name," (",pred_unit,")"),limits = c(min(data$Xfut),max(data$Xfut)),expand=c(0,0))+
+    theme_bw(base_size = 18)+
+    theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
+    theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+    scale_y_continuous(paste0("Changement moyen (",ind_unit,")"))+
+    ggtitle(paste0("Changement moyen et partition de variance pour\nle predicteur ",pred_name," et l'indicateur ",ind_name," (",bv_name,")"))
+  save.plot(plt,Filename = paste0("meanchange_variance_",ind_name,"_",pred_name,"_",bv_name),Folder = folder_out,Format = "jpeg")
+}
+
+##############################################################################
+## Time series of variance partition
+## returns a ggplot 2 object
+## QUALYPSOOUT a Qualypso output object (effects order should be coherent with color coding inside this function)
+## pred_name the plain language name of the predictor
+## ind_name the plain language name of the indicator
+## bv_name the plain language name of the watershed if needed
+## ind_unit the unit of the indicator
+## pred_unit the unit of the predictor
+
+plotQUALYPSOTotalVarianceDecomposition_ggplot=function(QUALYPSOOUT,pred_name,ind_name,bv_name,pred_unit,folder_out){
+  
+  Xfut = QUALYPSOOUT$Xfut
+  nFut = length(Xfut)
+  nEff = QUALYPSOOUT$listScenarioInput$nEff
+  vecEff = 1:nEff
+  VARDECOMP = QUALYPSOOUT$DECOMPVAR
+  VARDECOMP[, 1:nEff] = VARDECOMP[, vecEff]
+  cum = cum.smooth = rep(0, nFut)
+  data=VARDECOMP
+  for (i in 1:(nEff + 2)) {
+    cumPrevious = cum.smooth
+    cum = cum + VARDECOMP[, i]
+    cum.smooth = predict(loess(cum ~ Xfut))
+    cum.smooth[cum.smooth < 0] = 0
+    data[,i]=cum.smooth
+  }
+  data=data.frame(cbind(Xfut,data))
+  data=gather(data,key = "var",value = "val",-c(Xfut))
+  namesEff = QUALYPSOOUT$names
+  names_var=c("InternalVar","ResidualVar",rev(namesEff))
+  data$var=factor(data$var,levels=c(names_var))
+  
+  #These colors work only if the order of the effects does not move
+  col_7var=c("orange","yellow", "cadetblue1", "blue1","darkgreen", "darkgoldenrod4","darkorchid1")
+  legend_var=c("Var. Int.","Var. Res.","RCM","GCM","RCP","BC","HM")
+  
+  plt=ggplot(data)+
+    geom_ribbon(aes(x=Xfut,ymin=0,ymax=val,fill=var),alpha=0.8)+
+    scale_fill_discrete("Partition\nde la variance",type = col_7var[1:length(names_var)],labels=legend_var[1:length(names_var)])+
+    scale_x_continuous(paste0(pred_name," (",pred_unit,")"),limits = c(min(data$Xfut),max(data$Xfut)),expand=c(0,0))+
+    theme_bw(base_size = 18)+
+    theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
+    theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+    scale_y_continuous(paste0("Partition de variance"),limits = c(0,1.01),expand=c(0,0))+
+    ggtitle(paste0("Partition de variance pour\nle predicteur ",pred_name," et l'indicateur ",ind_name," (",bv_name,")"))
+  save.plot(plt,Filename = paste0("partition_variance_",ind_name,"_",pred_name,"_",bv_name),Folder = folder_out,Format = "jpeg")
+}
+
+##############################################################################
+## Time series of one scenario + uncertainties
+## returns a ggplot 2 object
+## QUALYPSOOUT a Qualypso output object (effects order should be coherent with color coding inside this function)
+## nameEff name of effect as in QUALYPSOOUT
+## nameScenario name of scenario as in nameEff
+## plain_name_Scen plain language name of scenario for legend
+## pred_name the plain language name of the predictor
+## ind_name the plain language name of the indicator
+## bv_name the plain language name of the watershed if needed
+## ind_unit the unit of the indicator
+## pred_unit the unit of the predictor
+
+plotQUALYPSOTotalVarianceByScenario_ggplot=function(QUALYPSOOUT,nameEff, nameScenario,plain_name_Scen,pred_name,ind_name,bv_name,ind_unit,pred_unit,folder_out){
+  
+  Xfut = QUALYPSOOUT$Xfut
+  nFut = length(Xfut)
+  iEff = which(QUALYPSOOUT$namesEff == nameEff)
+  if (length(iEff) == 0){stop("wrong value for nameEff")}
+  iScenario = which(QUALYPSOOUT$listScenarioInput$listEff[[iEff]] == 
+                      nameScenario)
+  meanPred = QUALYPSOOUT$CHANGEBYEFFECT[[nameEff]]$MEAN[,iScenario]
+  Veff = QUALYPSOOUT$EFFECTVAR[, -iEff]
+  Vbind = rbind(t(Veff), QUALYPSOOUT$RESIDUALVAR$MEAN, QUALYPSOOUT$INTERNALVAR)
+  nEff = nrow(Vbind) - 2
+  Vtot = colSums(Vbind)
+  Vnorm = Vbind/t(replicate(n = nrow(Vbind), Vtot))
+  vNormRev = apply(Vnorm, 2, rev)
+  probCI = QUALYPSOOUT$listOption$probCI
+  binf = qnorm(p = (1 - probCI)/2, mean = meanPred, sd = sqrt(Vtot))
+  bsup = qnorm(p = 0.5 + probCI/2, mean = meanPred, sd = sqrt(Vtot))
+  limIntInf = limIntSup = matrix(nrow = nEff + 2, ncol = nFut)
+  limIntInf[1, ] = predict(loess(binf ~ Xfut))
+  limIntSup[1, ] = predict(loess(bsup ~ Xfut))
+  for (i in 1:(nEff + 1)) {
+    binfi = limIntInf[i, ] + vNormRev[i, ] * (meanPred - 
+                                                binf)
+    limIntInf[i + 1, ] = predict(loess(binfi ~ Xfut))
+    bsupi = limIntSup[i, ] - vNormRev[i, ] * (bsup - meanPred)
+    limIntSup[i + 1, ] = predict(loess(bsupi ~ Xfut))
+  }
+  
+  namesEff = QUALYPSOOUT$names
+  namesEff=namesEff[namesEff!=nameEff]
+  names_var=c("int","res",rev(namesEff))
+  
+  
+  data=data.frame(Xfut=Xfut,mean=meanPred)
+  data=cbind(data,t(limIntInf))
+  colnames(data)[-c(1:2)]=names_var
+  data2=gather(data,key = "var",value = "inf",-c(Xfut,mean))
+  data=data.frame(Xfut=Xfut)
+  data=cbind(data,t(limIntSup))
+  colnames(data)[-c(1)]=names_var
+  data3=gather(data,key = "var",value = "sup",-Xfut)
+  data=merge(data2,data3,by=c("Xfut","var"))
+  data$var=factor(data$var,levels=c(names_var))
+  
+  #These colors work only if the order and the name of the effects does not move
+  col_7var=c("orange","yellow", "cadetblue1", "blue1","darkgreen", "darkgoldenrod4","darkorchid1")
+  legend_var=c("Var. Int.","Var. Res.","RCM","GCM","RCP","BC","HM")
+  legend_var_full=c("int","res","rcm","gcm","rcp","bc","hm")
+  legend_var=legend_var[-which(legend_var_full==nameEff)]
+  col_7var=col_7var[-which(legend_var_full==nameEff)]
+  
+  plt=ggplot(data)+
+    geom_ribbon(aes(x=Xfut,ymin=inf,ymax=sup,fill=var),alpha=0.8)+
+    geom_line(aes(x=Xfut,y=mean,color="black"))+
+    scale_fill_discrete("Partition\nde la variance",type = col_7var[1:length(names_var)],labels=legend_var[1:length(names_var)])+
+    scale_color_discrete("",type="black",label="Moyenne lissee")+
+    scale_x_continuous(paste0(pred_name," (",pred_unit,")"),limits = c(min(data$Xfut),max(data$Xfut)),expand=c(0,0))+
+    theme_bw(base_size = 18)+
+    theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
+    theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+    scale_y_continuous(paste0("Changement moyen (",ind_unit,")"))+
+    ggtitle(paste0("Changement et partition de variance pour les experiences ",plain_name_Scen,"\nle predicteur ",pred_name," et l'indicateur ",ind_name," (",bv_name,")"))
+  save.plot(plt,Filename = paste0("change_variance_",nameScenario,"_",ind_name,"_",pred_name,"_",bv_name),Folder = folder_out,Format = "jpeg")
+}
