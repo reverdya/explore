c6e494addbc3be7f581de17f980c4bb40427397e
ALIX REVERDY <reverdy.alix@gmail.com>
2022-04-25 16:52:46 +0200

    added lm method

--- run_Qualypso.R
+++ run_Qualypso.R
@@ -59,7 +59,8 @@ tmp=format_global_tas(path_data,first_full_year,last_full_year,simu_lst,first_re
 mat_Globaltas=tmp[[1]]
 ref_Globaltas=tmp[[2]]
 
-##Loop Qualypso on indexes
+################################
+## Linear method
 for (indc in lst_indic){
   
   all_chains=vector(length=nrow(simu_lst),mode="list")
@@ -78,16 +79,65 @@ for (indc in lst_indic){
     ClimateProjections=lapply(all_chains, function(x) x[,c(1,i)])
     ClimateProjections=lapply(ClimateProjections,function(x) x[x$year>=first_full_year & x$year<=last_full_year,][,2])
     Y=t(do.call(cbind,ClimateProjections))
+    
+    # call main function QUALYPSO
+    listOption = list(spar=spar,typeChangeVariable=typeChangeVar,ANOVAmethod="lm",nBurn=1000,nKeep=5000,nCluster=nbcore,
+                      parSmooth=spar,probCI=0.9,quantilePosterior =c(0.01,0.05,0.1,0.25,0.5,0.75,0.9,0.95,0.99))
+    ##Time predictor
+    lst.QUALYPSOOUT_time[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
+                                           scenAvail=scenAvail,
+                                           X = vecYears,
+                                           Xref = ref_year,
+                                           listOption=listOption)# no Xfut or iFut because we want all values
+    ##Temperature predictor    
+    lst.QUALYPSOOUT_temp[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
+                                           scenAvail=scenAvail,
+                                           X = mat_Globaltas,
+                                           Xref = ref_Globaltas,
+                                           Xfut=vec_futGlobaltas,
+                                           listOption=listOption)# no iFut because we want all values
+    
+    if(((i-1) %% 100)==0){print(i-1)}
+    
+  }
+  toc()  
+  
+  save(lst.QUALYPSOOUT_time,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_time_lm.RData"))
+  save(lst.QUALYPSOOUT_temp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp_lm.RData"))
+  
+}
 
+
+################################
+## QUALYPSO method
+for (indc in lst_indic){
+  
+  all_chains=vector(length=nrow(simu_lst),mode="list")
+  for (i in 1:nrow(simu_lst)){
+    load(paste0(path_data,"processed/indic_hydro/",indc,"_",simu_lst$rcp[i],"_",simu_lst$gcm[i],"_",simu_lst$rcm[i],"_",simu_lst$bc[i],"_",simu_lst$hm[i],".Rdata"))
+    all_chains[[i]]=res
+  }
+  
+  n_bv=ncol(all_chains[[1]])-1
+  lst.QUALYPSOOUT_time=vector(mode="list",length=n_bv)
+  lst.QUALYPSOOUT_temp=vector(mode="list",length=n_bv)
+  
+  tic()
+  for(i in 2:(n_bv+1)){
+    
+    ClimateProjections=lapply(all_chains, function(x) x[,c(1,i)])
+    ClimateProjections=lapply(ClimateProjections,function(x) x[x$year>=first_full_year & x$year<=last_full_year,][,2])
+    Y=t(do.call(cbind,ClimateProjections))
+    
     # call main function QUALYPSO
     listOption = list(spar=spar,typeChangeVariable=typeChangeVar,ANOVAmethod="QUALYPSO",nBurn=1000,nKeep=5000,nCluster=nbcore,
                       parSmooth=spar,probCI=0.9,quantilePosterior =c(0.01,0.05,0.1,0.25,0.5,0.75,0.9,0.95,0.99))
     ##Time predictor
     lst.QUALYPSOOUT_time[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
-                                      scenAvail=scenAvail,
-                                      X = vecYears,
-                                      Xref = ref_year,
-                                      listOption=listOption)# no Xfut or iFut because we want all values
+                                           scenAvail=scenAvail,
+                                           X = vecYears,
+                                           Xref = ref_year,
+                                           listOption=listOption)# no Xfut or iFut because we want all values
     ##Temperature predictor    
     lst.QUALYPSOOUT_temp[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
                                            scenAvail=scenAvail,
