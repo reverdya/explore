aed11ef1c9cd7d13b8ffe56ff260093e3db0c821
ALIX REVERDY <reverdy.alix@gmail.com>
2022-05-02 18:15:38 +0200

    keep only lm, correct temperature predictor method

--- run_Qualypso.R
+++ run_Qualypso.R
@@ -43,8 +43,6 @@ vecYears = seq(first_full_year,last_full_year,1)
 
 select_gcm=c("CNRM-CM5-LR","EC-EARTH","IPSL-CM5A-MR")
 
-#ref_GlobalTas=0
-vec_futGlobaltas=c(1,1.5,2,2.5,3,3.5,4,4.5,5)
 
 
 ######
@@ -71,7 +69,9 @@ for (indc in lst_indic){
   
   n_bv=ncol(all_chains[[1]])-1
   lst.QUALYPSOOUT_time=vector(mode="list",length=n_bv)
-  lst.QUALYPSOOUT_temp=vector(mode="list",length=n_bv)
+  lst.QUALYPSOOUT_temp_3rcp=vector(mode="list",length=n_bv)
+  lst.QUALYPSOOUT_temp_2rcp=vector(mode="list",length=n_bv)
+  lst.QUALYPSOOUT_temp_1rcp=vector(mode="list",length=n_bv)
   
   tic()
   for(i in 2:(n_bv+1)){
@@ -90,12 +90,29 @@ for (indc in lst_indic){
                                            Xref = ref_year,
                                            listOption=listOption)# no Xfut or iFut because we want all values
     ##Temperature predictor    
-    lst.QUALYPSOOUT_temp[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
+    lst.QUALYPSOOUT_temp_3rcp[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
                                            scenAvail=scenAvail,
                                            X = mat_Globaltas,
                                            Xref = ref_Globaltas,
-                                           Xfut=vec_futGlobaltas,
+                                           Xfut=seq(0.5,2,0.2),
                                            listOption=listOption)# no iFut because we want all values
+    
+    idx_2rcp=which(scenAvail$rcp=="rcp4.5"|scenAvail$rcp=="rcp8.5")
+    lst.QUALYPSOOUT_temp_2rcp[[i-1]] = QUALYPSO(Y=Y[idx_2rcp,], #one Y and run per basin because otherwise we cannot have several future times
+                                                scenAvail=scenAvail[idx_2rcp,],
+                                                X = mat_Globaltas[idx_2rcp,],
+                                                Xref = ref_Globaltas[idx_2rcp],
+                                                Xfut=seq(0.5,3,0.25),
+                                                listOption=listOption)# no iFut because we want all values
+    
+    ##For now this problem is under-constrained(3 + 4 effects + intercept for 6 chains)
+    # idx_1rcp=which(scenAvail$rcp=="rcp8.5")
+    # lst.QUALYPSOOUT_temp_1rcp[[i-1]] = QUALYPSO(Y=Y[idx_1rcp,], #one Y and run per basin because otherwise we cannot have several future times
+    #                                             scenAvail=scenAvail[idx_1rcp,c("gcm","rcm")],
+    #                                             X = mat_Globaltas[idx_1rcp,],
+    #                                             Xref = ref_Globaltas[idx_1rcp],
+    #                                             Xfut=seq(0.5,5,0.25),
+    #                                             listOption=listOption)# no iFut because we want all values
     
     if(((i-1) %% 100)==0){print(i-1)}
     
@@ -103,55 +120,8 @@ for (indc in lst_indic){
   toc()  
   
   save(lst.QUALYPSOOUT_time,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_time_lm.RData"))
-  save(lst.QUALYPSOOUT_temp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp_lm.RData"))
-  
+  save(lst.QUALYPSOOUT_temp_3rcp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp_3rcp_lm.RData"))
+  save(lst.QUALYPSOOUT_temp_2rcp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp_2rcp_lm.RData"))
+  #save(lst.QUALYPSOOUT_temp_1rcp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp_1rcp_lm.RData"))
 }
 
-
-################################
-## QUALYPSO method
-for (indc in lst_indic){
-  
-  all_chains=vector(length=nrow(simu_lst),mode="list")
-  for (i in 1:nrow(simu_lst)){
-    load(paste0(path_data,"processed/indic_hydro/",indc,"_",simu_lst$rcp[i],"_",simu_lst$gcm[i],"_",simu_lst$rcm[i],"_",simu_lst$bc[i],"_",simu_lst$hm[i],".Rdata"))
-    all_chains[[i]]=res
-  }
-  
-  n_bv=ncol(all_chains[[1]])-1
-  lst.QUALYPSOOUT_time=vector(mode="list",length=n_bv)
-  lst.QUALYPSOOUT_temp=vector(mode="list",length=n_bv)
-  
-  tic()
-  for(i in 2:(n_bv+1)){
-    
-    ClimateProjections=lapply(all_chains, function(x) x[,c(1,i)])
-    ClimateProjections=lapply(ClimateProjections,function(x) x[x$year>=first_full_year & x$year<=last_full_year,][,2])
-    Y=t(do.call(cbind,ClimateProjections))
-    
-    # call main function QUALYPSO
-    listOption = list(spar=spar,typeChangeVariable=typeChangeVar,ANOVAmethod="QUALYPSO",nBurn=1000,nKeep=5000,nCluster=nbcore,
-                      parSmooth=spar,probCI=0.9,quantilePosterior =c(0.01,0.05,0.1,0.25,0.5,0.75,0.9,0.95,0.99))
-    ##Time predictor
-    lst.QUALYPSOOUT_time[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
-                                           scenAvail=scenAvail,
-                                           X = vecYears,
-                                           Xref = ref_year,
-                                           listOption=listOption)# no Xfut or iFut because we want all values
-    ##Temperature predictor    
-    lst.QUALYPSOOUT_temp[[i-1]] = QUALYPSO(Y=Y, #one Y and run per basin because otherwise we cannot have several future times
-                                           scenAvail=scenAvail,
-                                           X = mat_Globaltas,
-                                           Xref = ref_Globaltas,
-                                           Xfut=vec_futGlobaltas,
-                                           listOption=listOption)# no iFut because we want all values
-    
-    if(((i-1) %% 100)==0){print(i-1)}
-    
-  }
-  toc()  
-  
-  save(lst.QUALYPSOOUT_time,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_time.RData"))
-  save(lst.QUALYPSOOUT_temp,file=paste0(path_data,"processed/qualypso/",indc,"_list_QUALYPSOOUT_3GCM_temp.RData"))
-  
-}
