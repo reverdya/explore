a5afc7e4d5573410099857c001e58872bfc1e9cd
ALIX REVERDY <reverdy.alix@gmail.com>
2022-05-09 19:14:29 +0200

    bugs, raw plot chains function temp.

--- check_indic.R
+++ check_indic.R
@@ -33,7 +33,7 @@ rcp=c("historical","rcp2.6","rcp4.5","rcp8.5")
 bc=c("ADAMONT")
 hm=c("SIM2")
 
-lst_indic=c("Q_mean_year","Q_q95_year","VCN10","VCN10_day")
+load(file=paste0(path_data,"processed/lst_indic.Rdata"))
 units=c("m3/s","m3/s","m3/s","day of year")
 
 last_full_year_smooth=2083#last year with complete data for a 30 years rolling mean and all chains
@@ -41,7 +41,10 @@ spar=1.1
 centr_ref_year=1990# central year of 1975-2005 reference period
 nb_year_mean=30
 typeChangeVar="rel"
-
+first_ref_year=1975
+last_ref_year=2005
+first_full_year=1972# from raw data filenames
+last_full_year=2098# from raw data filenames
 
 
 ###########
@@ -89,6 +92,7 @@ for (i in 1:length(lst_indic)){# for each indicator
     nr=ncol(res)-1
     
     #res_smooth=cbind(res[,1],apply(res[,-1],2,rollmean,k=nb_year_mean,align="center",fill=NA))#30 years rolling mean
+    res=res[res$year>=first_full_year&res$year<=last_full_year,]
     zz = !is.na(res[,2])
     vecYears=res[zz,1]
     #spline
@@ -174,7 +178,7 @@ for (i in 1:length(lst_indic)){# for each indicator
 
 
 ###############################################################################################
-## Plot raw indicator, and its spline for all models and selection of watersheds by RCP
+## Plot raw indicator, and its spline for all models and selection of watersheds by RCP for time
 ## Check for coherence of using spline and possible chains that are outlying
 ## checks particularly that data is not cyclical
 ## Climate response not climate change response
@@ -192,6 +196,7 @@ for (SPAR in c(0.5,0.8,0.9,1.0,1.1,1.2,1.5)){
       res=res[,c(1,select_stations$idx+1)]
       
       #res=cbind(res[,1],apply(res[,-1],2,rollmean,k=nb_year_mean,align="center",fill=NA))#30 years rolling mean
+      res=res[res$year>=first_full_year&res$year<=last_full_year,]
       zz = !is.na(res[,2])
       vecYears=res[zz,1]
       #spline
@@ -233,22 +238,22 @@ for (SPAR in c(0.5,0.8,0.9,1.0,1.1,1.2,1.5)){
           theme_bw(base_size = 18)+
           theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
           theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
-          ggtitle(paste0("Time series of absolute change of ",lst_indic[i],"\nfor ",r," at ",select_stations$Nom[w-1]))+
+          ggtitle(paste0("Time series of absolute change of ",lst_indic[i],"\nfor ",r," at ",select_stations$Nom[w]))+
           scale_x_continuous("")+
           scale_y_continuous(paste0("Climate response ( ",units[i]," )"))+
           guides(color = guide_legend(override.aes = list(size = 1.7)))+
           facet_wrap(vars(gcm))+
           theme(panel.spacing.x = unit(2, "lines"))
         if(SPAR==1){
-          save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w-1],"_",r,"_spar1.0"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
+          save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_spar1.0"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
           }else{#just to ease file sorting
-            save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w-1],"_",r,"_spar",SPAR),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
+            save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_spar",SPAR),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
           }
         # if(min(data$val,na.rm=T)>0){
         #   plt2=plt+coord_trans(y="log10")
-        #   save.plot(plt2,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w-1],"_",r,"_log"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
+        #   save.plot(plt2,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_log"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
         # }else{
-        #   print(paste0(lst_indic[i],"_chronique_",select_stations$Nom[w-1],"_",r,"_log is impossible because of null or negative values in spline"))
+        #   print(paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_log is impossible because of null or negative values in spline"))
         # }
         
       }
@@ -257,7 +262,96 @@ for (SPAR in c(0.5,0.8,0.9,1.0,1.1,1.2,1.5)){
   }
 }
 
+###############################################################################################
+## Plot raw indicator, and its spline for all models and selection of watersheds by RCP for temperature
+## Check for coherence of using spline and possible chains that are outlying
+## checks particularly that data is not cyclical
+## Climate response not climate change response
 
+tmp=format_global_tas(path_data,first_full_year,last_full_year,simu_lst,first_ref_year,last_ref_year)
+mat_Globaltas=tmp[[1]]
+
+##Merge data frame warnings are okay
+for (SPAR in c(0.5,0.8,0.9,1.0,1.1,1.2,1.5)){
+  for (i in 1:length(lst_indic)){# for each indicator
+    clim_resp=vector(length=nrow(simu_lst),mode="list")
+    clim_resp_spline=vector(length=nrow(simu_lst),mode="list")
+    dir.create(paste0(path_fig,lst_indic[i],"/plot_chains_temp/"))
+    for(c in 1:nrow(simu_lst)){# for each chain
+      
+      load(file = paste0(path_data,"processed/indic_hydro/",lst_indic[i],"_",simu_lst$rcp[c],"_",simu_lst$gcm[c],"_",simu_lst$rcm[c],"_",simu_lst$bc[c],"_",simu_lst$hm[c],".Rdata"))
+      res=res[,c(1,select_stations$idx+1)]
+      
+      #res=cbind(res[,1],apply(res[,-1],2,rollmean,k=nb_year_mean,align="center",fill=NA))#30 years rolling mean
+      res=res[res$year>=first_full_year&res$year<=last_full_year,]
+      zz = !is.na(res[,2])
+
+      #spline
+      res_spline=res
+      tas=mat_Globaltas[c,]
+      for(j in 2:ncol(res)){
+        res_spline[zz,j]=smooth.spline(x=tas,y=res[zz,j],spar = SPAR)$y
+      }
+      res=res[,-1]
+      res_spline=res_spline[,-1]
+      res$tas=tas
+      res_spline$tas=tas
+      clim_resp[[c]]=res
+      clim_resp_spline[[c]]=res_spline
+    }
+    
+    for (w in 1:(ncol(clim_resp[[1]]-1))){
+      for (r in lst_names_eff$rcp){
+        
+        chain_r=which(simu_lst$rcp==r)#chains with the right rcp
+        raw=clim_resp[[chain_r[1]]][,c(w,ncol(clim_resp[[1]]))]#first iteration outside loop
+        spline=clim_resp_spline[[chain_r[1]]][,c(w,ncol(clim_resp[[1]]))]
+        for (R in 2:length(chain_r)){
+          raw=merge(raw,clim_resp[[chain_r[R]]][,c(w,ncol(clim_resp[[1]]))],by="tas",all=T)
+          spline=merge(spline,clim_resp_spline[[chain_r[R]]][,c(w,ncol(clim_resp[[1]]))],by="tas",all=T)
+          ## Warnings okay
+        }
+        colnames(raw)[-1]=paste0(simu_lst[simu_lst$rcp==r,]$gcm,"_",simu_lst[simu_lst$rcp==r,]$rcm)
+        colnames(spline)[-1]=paste0(simu_lst[simu_lst$rcp==r,]$gcm,"_",simu_lst[simu_lst$rcp==r,]$rcm)
+        raw=gather(raw,key = "model",value = "val",-tas)
+        raw$type="raw"
+        spline=gather(spline,key = "model",value = "val",-tas)
+        spline$type="spline"
+        data=rbind(raw,spline)
+        data$gcm=unlist(lapply(strsplit(data$model,"_"),function(x) x[1]))
+        data$rcm=unlist(lapply(strsplit(data$model,"_"),function(x) x[2]))
+        data=data[!is.na(data$val),]
+        
+        plt=ggplot(data)+#Warnings okay
+          geom_line(aes(x=tas,y=val,size=type,color=rcm))+
+          scale_size_manual("",values=c(0.7,1.7),label=c("Climate response","Spline fit"))+
+          scale_color_manual("RCM",values=brewer.paired(length(unique(data$rcm))))+
+          theme_bw(base_size = 18)+
+          theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+          theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
+          ggtitle(paste0("Time series of absolute change of ",lst_indic[i],"\nfor ",r," at ",select_stations$Nom[w]))+
+          scale_x_continuous("Temperature (degC)")+
+          scale_y_continuous(paste0("Climate response ( ",units[i]," )"))+
+          guides(color = guide_legend(override.aes = list(size = 1.7)))+
+          facet_wrap(vars(gcm))+
+          theme(panel.spacing.x = unit(2, "lines"))
+        if(SPAR==1){
+          save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_spar1.0"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains_temp/"),Format = "jpeg")
+        }else{#just to ease file sorting
+          save.plot(plt,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_spar",SPAR),Folder = paste0(path_fig,lst_indic[i],"/plot_chains_temp/"),Format = "jpeg")
+        }
+        # if(min(data$val,na.rm=T)>0){
+        #   plt2=plt+coord_trans(y="log10")
+        #   save.plot(plt2,Filename = paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_log"),Folder = paste0(path_fig,lst_indic[i],"/plot_chains/"),Format = "jpeg")
+        # }else{
+        #   print(paste0(lst_indic[i],"_chronique_",select_stations$Nom[w],"_",r,"_log is impossible because of null or negative values in spline"))
+        # }
+        
+      }
+    }
+    
+  }
+}
 
 #################################################################################################################################
 ## Percentage of discharge value <0.001 (m3/s,days...)  and of outliers (chain of worst case for each basin) in indicators
