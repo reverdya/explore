ca0e4357771e612a07bce848ad02e09222a143dc
ALIX REVERDY <reverdy.alix@gmail.com>
2022-07-19 16:04:18 +0200

    spline, variables spelling

--- make_figures_qualypso.R
+++ make_figures_qualypso.R
@@ -22,7 +22,7 @@ source('C:/Users/reverdya/Documents/Docs/1_code/explore/general_functions.R',enc
 path_data="C:/Users/reverdya/Documents/Docs/2_data/"
 path_fig="C:/Users/reverdya/Documents/Docs/3_figures/Qualypso/"
 
-var=c("Debits")
+Var=c("Debits")
 rcp=c("historical","rcp2.6","rcp4.5","rcp8.5")
 bc=c("ADAMONT")
 hm=c("SIM2")
@@ -86,7 +86,7 @@ for (i in 1:length(lst_indic)){
       plotQUALYPSOeffect_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],nameEff="gcm",plain_nameEff = "GCM",pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,includeMean = T,xlim=xlim,incert = T)
       plotQUALYPSOeffect_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],nameEff="rcm",plain_nameEff = "RCM",pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,xlim=xlim,incert = T)
       plotQUALYPSOeffect_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],nameEff="rcm",plain_nameEff = "RCM",pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,includeMean = T,xlim=xlim,incert = T)
-      
+
       plotQUALYPSOMeanChangeAndUncertainties_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,xlim=xlim)
       plotQUALYPSOMeanChangeAndUncertainties_noIV_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,xlim=xlim,iv_type = "sum")
       plotQUALYPSOMeanChangeAndUncertainties_noIV_ggplot(QUALYPSOOUT = lst.QUALYPSOOUT[[idx]],pred=predict[p],pred_name = pred_name,ind_name = lst_indic[i],ind_name_full=name_indic[i],bv_name = select_stations$Nom[b],bv_full_name = select_stations$Nom_complet[b],pred_unit = pred_unit,folder_out=folder_out,xlim=xlim,iv_type = "tot")
@@ -138,7 +138,7 @@ for (i in 1:length(lst_indic)){
   }else{
       map_one_var(lst.QUALYPSOOUT = lst.QUALYPSOOUT,vartype="varres",horiz = horiz,pred_name = pred_name,pred = pred,pred_unit = pred_unit,ind_name = lst_indic[i],ind_name_full=name_indic[i],folder_out = folder_out,freq_col=freq_col)
   }
-  
+
   load(file=paste0("C:/Users/reverdya/Documents/Docs/2_data/processed/qualypso/",lst_indic[i],"_list_QUALYPSOOUT_3GCM_temp_3rcp_lm.RData"))
   load(file=paste0("C:/Users/reverdya/Documents/Docs/2_data/processed/qualypso/",lst_indic[i],"_list_QUALYPSOOUT_3GCM_temp_2rcp_lm.RData"))
   map_3quant_1.5_2_2.5_degC(lst.QUALYPSOOUT3 = lst.QUALYPSOOUT_temp_3rcp,lst.QUALYPSOOUT2 = lst.QUALYPSOOUT_temp_2rcp,ind_name = lst_indic[i],ind_name_full=name_indic[i],folder_out = folder_out,freq_col=freq_col)
@@ -155,9 +155,9 @@ for (i in 1:length(lst_indic)){
 tmp=format_global_tas(path_data,first_data_year,last_data_year,simu_lst,first_ref_year,last_ref_year)
 mat_Globaltas_gcm=tmp[[3]]
 mat_Globaltas_gcm=data.frame(apply(mat_Globaltas_gcm,MARGIN = 2,function(x) smooth.spline(x=seq(first_data_year,last_data_year),y=x,spar = 1)$y))
-mat_Globaltas_gcm$year=seq(first_data_year,last_data_year)
+#mat_Globaltas_gcm$year=seq(first_data_year,last_data_year)
 
-data=gather(mat_Globaltas_gcm,key = "chain",value = "val",-year)
+data=pivot_longer(data=mat_Globaltas_gcm,cols=!year,names_to = "chain",values_to = "val")
 data$rcp=unlist(lapply(strsplit(data$chain,"_"),function(x) x[1]))
 data$gcm=unlist(lapply(strsplit(data$chain,"_"),function(x) x[2]))
 
@@ -186,14 +186,15 @@ for ( i in 1:length(paths)){
   pre_indus_tas=mean(tas_glob$tas[tas_glob$year>=1860 & tas_glob$year<=1900])
   tas_glob$tas=tas_glob$tas-pre_indus_tas
   colnames(tas_glob)[2]=paste0(strsplit(strsplit(paths[i],"/")[[1]][9],"_")[[1]][5],"_",strsplit(strsplit(paths[i],"/")[[1]][9],"_")[[1]][4])#rcp_gcm name
-  tas_glob_full[[i]]=tas_glob[tas_glob$year<=2099,]
+  tas_glob_full[[i]]=tas_glob[tas_glob$year<=2100,]
 }
 mat_Globaltas=lapply(tas_glob_full,function(x) cbind(x[,1],smooth.spline(x=x[,1],y=x[,2],spar = 1)$y))
-mat_Globaltas=lapply(mat_Globaltas,function(x) x[x[,1]>=1861&x[,1]<=2100,2])
+mat_Globaltas=lapply(mat_Globaltas,function(x) x[x[,1]>=1861&x[,1]<=2100,2])# here 1861 because we wanna show GFDL
 mat_Globaltas=t(do.call(cbind,mat_Globaltas))
 years=seq(1861,2100)
+stock=mat_Globaltas
 
-data=gather(mat_Globaltas_gcm,key = "chain",value = "val",-year)
+data=pivot_longer(data=mat_Globaltas_gcm,cols=!year,names_to = "chain",values_to = "val")
 data$rcp=unlist(lapply(strsplit(data$chain,"_"),function(x) x[1]))
 data$gcm=unlist(lapply(strsplit(data$chain,"_"),function(x) x[2]))
 
@@ -217,14 +218,17 @@ idx=vector(mode = "list")
 temp_ref=c(1,1.5,2,3,4,5)
 for(temp in temp_ref){
   #stock is variable from previous chunk
-  idx[[as.character(temp)]]=apply(stock,MARGIN=2,function(x) min(which(x>=temp)))[-1]#-1 for year
+  idx[[as.character(temp)]]=apply(stock,MARGIN=1,function(x) min(which(x>=temp)))
 }
 
 idx=data.frame(do.call(rbind, idx))
 idx[idx==Inf]=NA
-idx=data.frame(apply(idx,MARGIN=2,function(x) stock$year[x]))
+idx=data.frame(apply(idx,MARGIN=2,function(x) years[x]))
 idx$temp=temp_ref
-data=gather(idx,key = "chain",value = "val",-temp)
+for(i in 1:length(paths)){
+  colnames(idx)[i]=paste0(strsplit(strsplit(paths[i],"/")[[1]][9],"_")[[1]][5],"_",strsplit(strsplit(paths[i],"/")[[1]][9],"_")[[1]][4])
+}
+data=pivot_longer(data=idx,cols=!temp,names_to = "chain",values_to = "val")
 data$rcp=unlist(lapply(strsplit(data$chain,"_"),function(x) x[1]))
 
 
@@ -254,6 +258,8 @@ for (i in 1:nrow(simu_lst)){
   mat_Globaltas[[i]]=mat_Globaltas_gcm[,which(vec_global_tas_gcm==simu_lst[i,]$gcm & vec_global_tas_rcp==sub(".","",simu_lst[i,]$rcp,fixed=T))]
 }
 mat_Globaltas=do.call(cbind,mat_Globaltas)
+mat_Globaltas=t(mat_Globaltas[seq(first_data_year,last_data_year) %in% seq(first_full_year,last_full_year),])
+mat_Globaltas_spline=mat_Globaltas_spline[,seq(first_data_year,last_data_year) %in% seq(first_full_year,last_full_year)]
 
 load(file=paste0("C:/Users/reverdya/Documents/Docs/2_data/processed/qualypso/Q_mean_year_list_QUALYPSOOUT_3GCM_time_lm.RData"))
 for (i in 1:nrow(select_stations)){
@@ -263,9 +269,11 @@ for (i in 1:nrow(select_stations)){
     all_chains[[j]]=res
   }
   ClimateProjections=lapply(all_chains, function(x) x[,c(1,select_stations$idx[i])])
+  ClimateProjections_spline=lapply(ClimateProjections, function(x) cbind(x[,1],smooth.spline(x=x[,1],y=x[,2],spar = 1.1)$y))
   ClimateProjections=lapply(ClimateProjections,function(x) x[x$year>=first_full_year & x$year<=last_full_year,][,2])
+  ClimateProjections_spline=lapply(ClimateProjections_spline,function(x) x[x[,1]>=first_full_year & x[,1]<=last_full_year,][,2])
   ClimateProjections=t(do.call(rbind,ClimateProjections))
-  ClimateProjections_spline=apply(ClimateProjections,MARGIN = 2,function(x) smooth.spline(x=vecYears,y=x,spar = 1.1)$y)
+  ClimateProjections_spline=t(do.call(rbind,ClimateProjections_spline))
   data=data.frame(tas=as.vector(mat_Globaltas),tas_spline=as.vector(mat_Globaltas_spline),q=as.vector(ClimateProjections),q_spline=as.vector(ClimateProjections_spline))
   
   plt=ggplot(data)+
@@ -278,7 +286,7 @@ for (i in 1:nrow(select_stations)){
     theme( axis.line = element_line(colour = "black"),panel.border = element_blank())+
     theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
     guides(colour = guide_legend(override.aes = list(size=3)))+
-    ggtitle(paste0("Module annuel du bassin ",select_stations$Nom[i],"\nen fonction du changement de temperature planetaire"))
+    ggtitle(paste0("Module annuel du bassin ",select_stations$Nom[i],"\nen fonction du changement de temperature planetaire (",first_full_year,"/",last_full_year,")"))
   save.plot(plt,Filename = paste0("global_tasVSmodule_",select_stations$Nom[i]),Folder = path_fig,Format = "jpeg")
 }
 
@@ -286,8 +294,8 @@ for (i in 1:nrow(select_stations)){
 ## Plot map of reference (1990) value of indicator for continuous positive indicator (of discharge)
 
 ref_year=1990
-lst_indic2=lst_indic[!lst_indic %in% c("log10VCN10","VCN10_day")]
-name_indic2=name_indic[!lst_indic %in% c("log10VCN10","VCN10_day")]
+lst_indic2=lst_indic[!lst_indic %in% c("VCN10_day")]
+name_indic2=name_indic[!lst_indic %in% c("VCN10_day")]
 for (i in 1:length(lst_indic2)){
     load(file=paste0("C:/Users/reverdya/Documents/Docs/2_data/processed/qualypso/",lst_indic2[i],"_list_QUALYPSOOUT_3GCM_time_lm.RData"))
     lst.QUALYPSOOUT=lst.QUALYPSOOUT_time
