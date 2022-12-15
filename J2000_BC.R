2f173e49e20d957f089fa46b9dbb88b4bc587c3c
ALIX REVERDY <reverdy.alix@gmail.com>
2022-12-15 12:47:28 +0100

    new dataset

--- J2000_BC.R
+++ J2000_BC.R
@@ -27,42 +27,72 @@ path_fig="C:/Users/reverdya/Documents/Docs/3_figures/meteo/analyse-indic/compare
 #MAIN#
 ######
 
-load(paste0(path_data,"J2000_CNRMCM5LR_ALADIN63_hist_3stations.Rdata"))
+load(paste0(path_data,"J2000_hist_5stations_2.Rdata"))
 head(dataJ2000)
 dim(dataJ2000)
-station=unique(dataJ2000$Station)
-forcage=unique(dataJ2000$forcage)
+print(unique(dataJ2000$Station))
+print(unique(dataJ2000$gcm))
+print(unique(dataJ2000$rcm))
+print(unique(dataJ2000$bc))
+dataJ2000$model=paste0(dataJ2000$gcm,"/",dataJ2000$rcm)
 dataJ2000$Station_name=dataJ2000$Station
 dataJ2000$Station_name[dataJ2000$Station_name=="M6240010"]="Loire à Nantes"
 dataJ2000$Station_name[dataJ2000$Station_name=="L3200610"]="Vienne à Ingrandes"
 dataJ2000$Station_name[dataJ2000$Station_name=="M0680610"]="Sarthe à St Denis d'Anjou"
+dataJ2000$Station_name[dataJ2000$Station_name=="L8000020"]="Loire à Saumur"
+dataJ2000$Station_name[dataJ2000$Station_name=="K1930010"]="Loire à Nevers"
 
-# dataJ2000$year=year(dataJ2000$Time)
-# Normalize by year mean?
+duplicata=dataJ2000[dataJ2000$model=="safran/safran",]# duplicate curve safran
+duplicata$model=paste0(unique(dataJ2000$gcm)[2],"/",unique(dataJ2000$rcm)[2])
+dataJ2000[dataJ2000$model=="safran/safran",]$model=paste0(unique(dataJ2000$gcm)[3],"/",unique(dataJ2000$rcm)[3])
+dataJ2000=rbind(dataJ2000,duplicata)
 
-##Density plot
+dataJ2000$Station_name=factor(dataJ2000$Station_name,levels=c("Loire à Nantes","Loire à Saumur","Loire à Nevers","Vienne à Ingrandes","Sarthe à St Denis d'Anjou"))
+
+
+##Density plot with log10 x-axis
 
 plt=ggplot(dataJ2000)+
-  geom_density(aes(x=Runoff,color=forcage,fill=forcage),alpha=0.4)+
-  facet_wrap(~Station_name, scales = "free")+
-  scale_color_viridis_d()+
-  scale_fill_viridis_d()+
-  scale_x_continuous(trans = "log10")+
+  geom_density(aes(x=Runoff,color=bc,fill=bc),alpha=0.4)+
+  facet_grid(model~Station_name, scales = "free_x")+
+  scale_color_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_fill_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_x_continuous("Runoff (m3/s)",trans = "log10",expand = expansion(mult = c(0, 0),add = c(0.2, 0)))+
+  scale_y_continuous("Density (KDE)")+
   theme_bw(base_size = 18)+
   theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
   theme( axis.line = element_line(colour = "black"))+
-  ggtitle("historical/CNRM-CM5/ALADIN63 (1976-2005)")
+  ggtitle("Historical (1976-2005)")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  theme(strip.text.x = element_text(size = 10))
 plt
 save.plot(plt,Filename = paste0("comparaisonBC_J2000"),Folder = path_fig,Format = "jpeg") 
 
-##Density plot woth year mean removed
+## Sorted discharge curve
+
+plt=ggplot(dataJ2000)+
+  stat_ecdf(aes(x=Runoff,color=bc),geom="step",size=1.5,alpha=0.8)+
+  facet_grid(model~Station_name, scales = "free_x")+
+  scale_color_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_x_continuous("Runoff (m3/s)",trans = "log10")+
+  scale_y_continuous("Fréquence au non-dépassement")+
+  theme_bw(base_size = 18)+
+  theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+  theme( axis.line = element_line(colour = "black"))+
+  ggtitle("Courbe des débit classés\nRuns historical (1976-2005)")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  theme(strip.text.x = element_text(size = 10))
+plt
+save.plot(plt,Filename = paste0("comparaisonBC_J2000_ecdf"),Folder = path_fig,Format = "jpeg") 
+
+##Density plot with year mean removed
 
 dataJ2000$year=year(dataJ2000$Time)
-data_year=aggregate(dataJ2000$Runoff,by=list(dataJ2000$year,dataJ2000$forcage,dataJ2000$Station),mean)
-colnames(data_year)=c("year","forcage","Station","Runoff")
+data_year=aggregate(dataJ2000$Runoff,by=list(dataJ2000$year,dataJ2000$model,dataJ2000$Station),mean)
+colnames(data_year)=c("year","model","Station","Runoff")
 dataJ2000$Runoff_detrend=dataJ2000$Runoff
 for(i in 1:nrow(dataJ2000)){
-  dataJ2000$Runoff_detrend[i]=dataJ2000$Runoff[i]-data_year[data_year$year==dataJ2000$year[i]&data_year$Station==dataJ2000$Station[i]&data_year$forcage==dataJ2000$forcage[i],"Runoff"]
+  dataJ2000$Runoff_detrend[i]=dataJ2000$Runoff[i]-data_year[data_year$year==dataJ2000$year[i]&data_year$Station==dataJ2000$Station[i]&data_year$model==dataJ2000$model[i],"Runoff"]
 }
 
 
@@ -70,15 +100,35 @@ trans_log10neg <- trans_new(name = "log10neg",transform = function(x) log10(x+20
 
 
 plt=ggplot(dataJ2000)+
-  geom_density(aes(x=Runoff_detrend,color=forcage,fill=forcage),alpha=0.4)+
-  facet_wrap(~Station_name, scales = "free")+
-  scale_color_viridis_d()+
-  scale_fill_viridis_d()+
-  scale_x_continuous("Runoff anomalies",trans=trans_log10neg,breaks = c(-1000,0,500,1000,5000),labels=c(-1000,0,500,1000,5000))+
-  scale_y_continuous(trans="sqrt")+
+  geom_density(aes(x=Runoff_detrend,color=bc,fill=bc),alpha=0.4)+
+  facet_grid(model~Station_name, scales = "free_x")+
+  scale_color_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_fill_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_x_continuous("Runoff anomalies (m3/s)",trans=trans_log10neg,breaks = c(-1000,0,500,1000,5000),labels=c(-1000,0,500,1000,5000),expand = expansion(mult = c(0, 0),add = c(0.05, 0.03)))+
+  scale_y_continuous("Density (KDE)",trans="sqrt")+
   theme_bw(base_size = 18)+
   theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
   theme( axis.line = element_line(colour = "black"))+
-  ggtitle("historical/CNRM-CM5/ALADIN63 (1976-2005)\n(yearly mean removed)")
+  ggtitle("Historical (1976-2005)\n(yearly mean removed)")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  theme(strip.text.x = element_text(size = 10))
 plt
 save.plot(plt,Filename = paste0("comparaisonBC_J2000_detrend"),Folder = path_fig,Format = "jpeg") 
+
+
+## Sorted discharge curve with mean year removed
+
+plt=ggplot(dataJ2000)+
+  stat_ecdf(aes(x=Runoff_detrend,color=bc),geom="step",size=1.5,alpha=0.8)+
+  facet_grid(model~Station_name, scales = "free_x")+
+  scale_color_viridis_d("",labels=c("ADAMONT","R2D2-2L","SAFRAN"))+
+  scale_x_continuous("Runoff anomalies (m3/s)",trans=trans_log10neg,breaks = c(-1000,0,500,1000,5000),labels=c(-1000,0,500,1000,5000),expand = expansion(mult = c(0, 0),add = c(0.05, 0.03)))+
+  scale_y_continuous("Fréquence au non-dépassement")+
+  theme_bw(base_size = 18)+
+  theme(plot.title = element_text( face="bold",  size=20,hjust=0.5))+
+  theme( axis.line = element_line(colour = "black"))+
+  ggtitle("Courbe des débit classés (soustraits de la moyenne annuelle)\nRuns historical (1976-2005)")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  theme(strip.text.x = element_text(size = 10))
+plt
+save.plot(plt,Filename = paste0("comparaisonBC_J2000_detrend_ecdf"),Folder = path_fig,Format = "jpeg") 
