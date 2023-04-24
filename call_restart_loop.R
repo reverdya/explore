70339f4ab9d5f9d9518a763770eb41315d319674
ALIX REVERDY <reverdy.alix@gmail.com>
2023-04-24 15:31:32 +0200

    version for sharing

--- call_restart_loop.R
+++ call_restart_loop.R
@@ -1,33 +1,42 @@
-###############################################
+##############################################
 ##Self-calling a new iteration after restart##
-###############################################
+##############################################
+# Alix Reverdy
+# 24/04/2023
 
 rm(list=ls())
 gc()
-library(rstudioapi)
+library(rstudioapi)#works only in Rstudio
 
-FCT=toto=function(CPT=1){#need to name an exemplar of the function FCT and to have only one parameter with default CPT=1
-  df=matrix(data=CPT,nrow = 1e5,ncol=1e4)
+## the heavy function to be ran (here an example with a heavy dataframe to be overwritten)
+toto=function(CPT){#need to have a function that will loop on a counter
+  df=matrix(data=CPT,nrow = 1e5,ncol=1e4)# watch out this is 7Go !!!!
   print(memory.size()/1024)
 }
 
-restart_loop=function(fct,cpt=1,n_iter,step){
-  fct #the heavy function to be ran 
+#######################################
+## The looping and restart function
+## fct the heavy function
+## cpt the first iteration (do not change default=1)
+## last the last iteration
+## step the step to which have the restart launched
+
+restart_loop=function(fct,cpt=1,last,step){
+  fct(cpt) 
   print(cpt)
   if(cpt==1){
     assign("STEP",step,envir = globalenv())
-    assign("N_ITER",n_iter,envir = globalenv())
-    
+    assign("LAST",last,envir = globalenv())
+    assign("FCT",fct,envir = globalenv())
   }
   assign("CPT",cpt+1,envir = globalenv())
-  if((cpt)!=n_iter){
+  if((cpt)!=last){
     if(cpt%%step==0){
-      restartSession(command="restart_loop(fct=FCT(CPT),cpt=CPT,n_iter=N_ITER,step=STEP)")#using only stuff in global environment
+      restartSession(command="restart_loop(fct=FCT,cpt=CPT,last=LAST,step=STEP)")#using only stuff in global environment
     }else{
-      restart_loop(fct=FCT(CPT),cpt=CPT,n_iter=N_ITER,step=STEP)
+      restart_loop(fct=FCT,cpt=CPT,last=LAST,step=STEP)
     }
-    
   }
 }
 
-restart_loop(fct=toto(),n_iter=10000,step=2)
+restart_loop(fct=toto,last=10000,step=2)
