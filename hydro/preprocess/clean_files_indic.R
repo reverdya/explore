e36007d91e829bcd3cd1639983581e8e9bb24b22
ALIX REVERDY <reverdy.alix@gmail.com>
2023-05-24 17:03:17 +0200

    latest version of data

--- hydro/preprocess/clean_files_indic.R
+++ hydro/preprocess/clean_files_indic.R
@@ -48,11 +48,11 @@ names(simu_lst)=c("indic","rcp","gcm","rcm","bc","hm","hm_domain","hm_NBbc")
 for (i in indic){
   for(h in 1:length(hm)){
     lst_f=list.dirs(paste0(path_data,"indic/",hm[h],"/"),recursive=F,full.names = F) #list all files of projections in folder
-    simu_lst$gcm=c(simu_lst$gcm,sapply(strsplit(lst_f," "),"[",1))
-    simu_lst$rcp=c(simu_lst$rcp,sapply(strsplit(lst_f," "),"[",2))
-    simu_lst$rcm=c(simu_lst$rcm,sapply(strsplit(lst_f," "),"[",3))
-    simu_lst$bc=c(simu_lst$bc,sapply(strsplit(lst_f," "),"[",4))
-    n=length(sapply(strsplit(lst_f," "),"[",1))
+    simu_lst$gcm=c(simu_lst$gcm,sapply(strsplit(lst_f,"_"),"[",1))
+    simu_lst$rcp=c(simu_lst$rcp,sapply(strsplit(lst_f,"_"),"[",2))
+    simu_lst$rcm=c(simu_lst$rcm,sapply(strsplit(lst_f,"_"),"[",3))
+    simu_lst$bc=c(simu_lst$bc,sapply(strsplit(lst_f,"_"),"[",4))
+    n=length(sapply(strsplit(lst_f,"_"),"[",1))
     simu_lst$indic=c(simu_lst$indic,rep(i,n))
     simu_lst$hm=c(simu_lst$hm,rep(hm[h],n))
     simu_lst$hm_domain=c(simu_lst$hm_domain,rep(hm_domain[h],n))
@@ -60,7 +60,6 @@ for (i in indic){
   }
 }
 simu_lst=data.frame(simu_lst)
-#simu_lst=simu_lst[!(simu_lst$gcm=="IPSL-CM5A-MR"&simu_lst$rcm=="WRF381P"),]
 
 
 ####################
@@ -71,14 +70,11 @@ scenAvail=simu_lst[simu_lst$indic=="QA",]
 tmp="toto"
 for(c in 1:nrow(scenAvail)){# for each chain
   dir_tmp <- list.files(paste0(path_data,"indic"), recursive = TRUE, include.dirs = TRUE,full.names = T,pattern =glob2rx(paste0("*",scenAvail$gcm[c],"*",scenAvail$rcp[c],"*",scenAvail$rcm[c],"*",scenAvail$bc[c],"*",scenAvail$hm[c],"*")))
-  file.copy(dir_tmp[2],paste0(path_data,"indic/",scenAvail$hm[c],"/"),recursive=T)
-  unlink(dir_tmp[1],recursive = T)
-  dir_tmp <- list.files(paste0(path_data,"indic"), recursive = TRUE, include.dirs = TRUE,full.names = T,pattern =glob2rx(paste0("*",scenAvail$gcm[c],"*",scenAvail$rcp[c],"*",scenAvail$rcm[c],"*",scenAvail$bc[c],"*",scenAvail$hm[c],"*")))
   if(tmp!=scenAvail$hm[c]){
     file.copy(paste0(dir_tmp,"/meta.fst"),paste0(dirname(dir_tmp),"/meta1.fst"))
   }
   tmp=scenAvail$hm[c]
   pth_tmp=Sys.glob(paths=paste0(dir_tmp,"/*.fst"))
-  file.remove(pth_tmp)#need to copy one of the
+  file.remove(pth_tmp)
   unlink(paste0(dir_tmp,"/dataEX_Explore2_proj_check"),recursive = T)
 }
